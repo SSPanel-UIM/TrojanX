cf886155bccaf3a3ef35f62fc2ca0a6809a4143a
Irohaede <irohaede@proton.me>
2022-07-24 16:14:50 +0000

    fix: tcp session payload write zero

--- src/server/session.rs
+++ src/server/session.rs
@@ -74,9 +74,11 @@ where
     where
         IO: AsyncRead + AsyncWrite + Unpin,
     {
+        if !self.payload.is_empty() {
+            let _ = self.socket.write(self.payload).await?;
+        }
         self.ctrl.consume_tx(self.payload.len());
-        let fut = poll_fn(|cx| self.ctrl.poll_pause(cx));
-        tokio::join!(fut, self.socket.write(self.payload)).1?;
+        poll_fn(|cx| self.ctrl.poll_pause(cx)).await;
         let mut stream = StreamWrapper::new(stream, self.ctrl);
         tokio::io::copy_bidirectional(&mut stream, &mut self.socket).await?;
         Ok(())
